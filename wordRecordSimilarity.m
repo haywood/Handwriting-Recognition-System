f510f9d269d27cc3b927f2c2d18560d0377d33d5
Michael Reed <mhr2126@gmail.com>
2011-12-10 00:47:46 -0500

    changed similarity function to make modifications easier

--- wordRecordSimilarity.m
+++ wordRecordSimilarity.m
@@ -4,33 +4,34 @@ function [ wordSim ] = wordRecordSimilarity(wordRecord1, wordRecord2)
 % Similarity is computed as an average of
 % cross-correlation results over the window sets of the two word records
 
-wordSim=0;
-total=0;
-
 windowList1=getField(wordRecord1, 'windowList');
 windowList2=getField(wordRecord2, 'windowList');
+simArray=[];
 
 
 for wIndex1=1:length(windowList1)
     
     window1=windowList1(wIndex1).window;
+
+    if max(window1(:)) ~= 0
     
-    for wIndex2=1:length(windowList2)
-    
-        window2=windowList2(wIndex2).window;
-        windowSim=-1;
-    
-        if max(window1(:)) ~= 0 && max(window2(:)) ~= 0        
-            windowSim=max([windowSim corr2(window1, window2)]);        
+        for wIndex2=1:length(windowList2)
+            
+            window2=windowList2(wIndex2).window;
+            windowSim=-1;
+            
+            if max(window2(:)) ~= 0
+                windowSim=max([windowSim corr2(window1, window2)]);
+            end
+            
+            if windowSim >= 0
+                simArray=[simArray windowSim];
+            end
+            
         end
-        
-        if windowSim >= 0
-            wordSim=wordSim+windowSim;
-            total=total+1;
-        end
-        
+    
     end
     
 end
 
-if total ~= 0 wordSim=wordSim/total; end
+wordSim=mean(simArray);
