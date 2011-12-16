0166e227047cfd92db9711bdd2c484e3bf6d05c5
Michael H Reed <mhr2126@gmail.com>
2011-12-16 00:14:53 -0500

    added script for downloading desired word images when all cannot be downloaded at once

---
+++ data/downloadWords.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my (@line,$wordIndex,$hostRoot,$localRoot,$filePath,$wordurls);
+
+$hostRoot='http://www.iam.unibe.ch/~fkiwww/iamDB/data/words/';
+$wordIndex='wordswithwriters.txt';
+$wordurls='wordurls.txt';
+$localRoot='words/';
+
+
+open INDEX, $wordIndex;
+open URLS, '>', $wordurls;
+
+while (<INDEX>) {
+    @line=split /\s+/;
+    print URLS $hostRoot, $line[4], "\n";
+}
+
+close URLS;
+close INDEX;
+
+`wget --http-user=mhr2126 --http-passwd=eureka7 -x -nH --cut-dirs=3 -i $wordurls`;
